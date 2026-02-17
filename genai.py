fb24bd2194557d8188929b8de0508b7cc8294a6c
SHASHWAT MAHAJAN <shashwatmahajan2@gmail.com>
2026-02-17 15:32:36 +0530

    Add files via upload

---
+++ genai.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.text_input("What's your name?")
+st.text_area("Write something...")
+st.number_input("Pick a number", min_value=0, max_value=100)
+st.slider("Choose a range", 0, 100)
+st.selectbox("Select a fruit", ["Apple", "Banana", "Mango"])
+st.multiselect("Choose toppings", ["Cheese", "Tomato", "Olives"])
+st.radio("Pick one", ["Option A", "Option B"])
+st.checkbox("I agree to the terms")
+
+if st.checkbox("Show Details"):
+    st.info("Here are more details...")
+
+
+
+with st.form("login_form"):
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    submitted = st.form_submit_button("Login")
+
+if submitted:
+    st.success(f"Welcome, {username}!")
